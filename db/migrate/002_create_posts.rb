5fda7b74a8f0db3f186f48117bf451f1ccd4b9b9
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-11 16:28:19 +0000

    Añadir timestamps

--- db/migrate/002_create_posts.rb
+++ db/migrate/002_create_posts.rb
@@ -4,6 +4,10 @@ migration 2, :create_posts do
       column :id, Integer, :serial => true
       column :title, DataMapper::Property::String, :length => 255
       column :body, DataMapper::Property::Text
+      column :created_at, DataMapper::Property::Datetime
+      column :created_on, DataMapper::Property::Date
+      column :updated_at, DataMapper::Property::Datetime
+      column :updated_on, DataMapper::Property::Date
     end
   end
 
